234c158b9a592dd867f999c770225e600b85c77f
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-06-27 19:40:36 -0400

    dev

--- BankApp/src/wiedman/tony/service/Lib.java
+++ BankApp/src/wiedman/tony/service/Lib.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.text.DecimalFormat;
-import wiedman.tony.bank.SQL;
+
 import wiedman.tony.models.User;
 
 public abstract class Lib implements AccountService {
