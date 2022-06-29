0f5f5a618f92a4c84e5c464a6cb09482bd66222b
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-06-29 13:33:32 -0400

    FORMATTING

--- BankApp/src/wiedman/tony/bank/Main.java
+++ BankApp/src/wiedman/tony/bank/Main.java
@@ -22,89 +22,82 @@ public class Main {
 		boolean usingBank = true;
 		
 			while (usingBank) {
-				
 				if (!user.isLoggedin()) {		
 					
 					System.out.println(
-									"----------------------------------------------------\n" 
-									+ " * MyBank Inc. - Welcome Back!\n"
-									+ "----------------------------------------------------\n\n" 
-									+ " 1.] Login to MyBank" + "\n"
-									+ " 2.] Open an Account ($25 Min. Deposit)" 
-									+ "\n" 
-									+ "\n" 
-									+ " 3.] Exit Bank" 
-									+ "\n"
-									+ "\n Type an option:");
+						"----------------------------------------------------\n" 
+						+ " * MyBank Inc. - Welcome Back!\n"
+						+ "----------------------------------------------------\n\n" 
+						+ " 1.] Login to MyBank" + "\n"
+						+ " 2.] Open an Account ($25 Min. Deposit)" 
+						+ "\n" 
+						+ "\n" 
+						+ " 3.] Exit Bank" 
+						+ "\n"
+						+ "\n Type an option:");
 				
 					String input = scanner.nextLine();
 
-						switch (input) {
-				
-							case "1":
-								// Open the login menu
-								logIn();
+					switch (input) {
+					
+						case "1":
+							logIn();
+							break;	
+							
+						case "2":
+							openAccount();
 							break;
-					
-							case "2":
-								// Open the account registration menu
-								openAccount();
-							break;
-					
-							default:
-								usingBank = false;
-								System.out.println("Thank you for banking with MyBank Inc.");
-					
-						}
+							
+						default:
+							usingBank = false;
+							System.out.println("Thank you for banking with MyBank Inc.");
+					}
 			}
 				
 			//(ACCOUNT MENU)
 			else {
 				
 				System.out.println(	
-								  "----------------------------------------------------\n" 
-								+ " * MyBank Inc. - Your Account\n"
-								+ "----------------------------------------------------\n" 
-								+ " Welcome back, " + user.getName()+"\n" 
-								+ "\n Account No: "+user.getAccountNumber()+" - "+user.getAccountType()+" Account\n"
-								+ "\n Account Balance: $" + user.getBalance() 
-								+ "\n\n" 
-								+ " 1.] Make Deposit" 
-								+ "\n"
-								+ " 2.] Make Withdrawal"
-								+ "\n" 
-								+ "\n" 
-								+ " 3.] Logout" 
-								+ "\n" 
-								+ "\nType an option:");
+					"----------------------------------------------------\n" 
+					+ " * MyBank Inc. - Your Account\n"
+					+ "----------------------------------------------------\n" 
+					+ " Welcome back, " + user.getName()+"\n" 
+					+ "\n Account No: "+user.getAccountNumber()+" - "+user.getAccountType()+" Account\n"
+					+ "\n Account Balance: $" + user.getBalance() 
+					+ "\n\n" 
+					+ " 1.] Make Deposit" 
+					+ "\n"
+					+ " 2.] Make Withdrawal"
+					+ "\n" 
+					+ "\n" 
+					+ " 3.] Logout" 
+					+ "\n" 
+					+ "\nType an option:");
 
 				String input = scanner.nextLine();
 				
 
 					switch (input) {
 						case "1":
-							
-							System.out.println("Enter Deposit Amount: ");
+								System.out.println("Enter Deposit Amount: ");
 							double depositAmount = scanner.nextDouble();
 							
-							user.makeDeposit(user, depositAmount);
-							
-						break;
+								user.makeDeposit(user, depositAmount);
+								
+							break;
 						
 						case "2":
-							
-							System.out.println("Enter Withdrawal Amount: ");
+								System.out.println("Enter Withdrawal Amount: ");
 							double withdrawAmount = scanner.nextDouble();
 							
-							user.makeWithdraw(user, withdrawAmount);
+								user.makeWithdraw(user, withdrawAmount);
 							
-						break;
+							break;
 						
 						case "3":
-							
-							user.setLoggedin(false);
-							
 							System.out.println("Logging out.");
+							
+								user.setLoggedin(false);
 							
 					}
 					
@@ -124,12 +117,12 @@ public class Main {
 		
 			while (loggingIn) {
 				
-				System.out.println("Username: ");
+					System.out.println("Username: ");
 				String username = scanner.nextLine();
 				
 					user.setUsername(username);
 					
-				System.out.println("Password: ");
+					System.out.println("Password: ");
 				String password = scanner.nextLine();
 				
 					user.setPassword(password);
@@ -162,43 +155,38 @@ public class Main {
 			while (openingAccount) {
 				
 				System.out.println(
-								  "Account Type: \n"
-								+ " 1.] Checking\n"
-								+ " 2.] Savings");
+					"Account Type: \n"
+					+ " 1.] Checking\n"
+					+ " 2.] Savings");
 			
 				String type = scanner.nextLine();
 			
 					switch (type) {
 					
 						case "1":
-							
 							user.setAccountType("Checking");
+							break;
 							
-						break;
-							
-						case "2":
-							
+						case "2":	
 							user.setAccountType("Savings");
-							
-						break;
-			}
+							break;
+					}
 					
-				System.out.println("Enter your full name:");
+					System.out.println("Enter your full name:");
 				String nameInput = scanner.nextLine();
 			
-				System.out.println("Enter a username: ");
+					System.out.println("Enter a username: ");
 				String usernameInput = scanner.nextLine();
 			
-				System.out.println("Enter a password: ");
+					System.out.println("Enter a password: ");
 				String passwordInput = scanner.nextLine();
 			
-				System.out.println("Type \"y\" to confirm, \"n\" to reset registration, any other key to return.");
+					System.out.println("Type \"y\" to confirm, \"n\" to reset registration, any other key to return.");
 				String confirmInput = scanner.nextLine().toLowerCase();
 
 					switch (confirmInput) {
 					
 						case "y":
-							
 							user.setName(nameInput);
 							user.setUsername(usernameInput);
 							user.setPassword(passwordInput);
@@ -207,19 +195,14 @@ public class Main {
 								user.createAccount(user);
 								
 							openingAccount = false;
-				
-						break;
+							break;
 						
 						case "n":
-							
 							System.out.println("Starting account application over.");
-							
-						break;
+							break;
 						
-						default:
-							
+						default:						
 							System.out.println("Returning to main menu.");
-							
 							openingAccount = false;
 					}
 					
