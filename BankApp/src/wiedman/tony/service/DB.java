6c7a277aa7a1df29058c054afbfb1f5b555d1210
tonywied17 <107732340+tonywied17@users.noreply.github.com>
2022-06-28 21:07:09 -0400

    2 Files

--- BankApp/src/wiedman/tony/service/DB.java
+++ BankApp/src/wiedman/tony/service/DB.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DB {
 	static final String DB_URL = "jdbc:postgresql://bankapp.cwhrhowdulyu.us-east-1.rds.amazonaws.com:5432/postgres";
 	static final String USER = "postgres";
 	static final String PASS = "Q!w2e3r4t5";
 	Connection connection = null;
-	//Create a DB connection
+
+	// Create a DB connection
 	public void connectDB() {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -21,7 +21,8 @@ public class DB {
 			System.exit(0);
 		}
 	}
-	//Close DB connection
+
+	// Close DB connection
 	public void closeDB() throws SQLException {
 		connection.close();
 	}
